64b3d4ae3fdfc5408503bdacff3b9d510a05019c
Naayeon <naaay097@gmail.com>
2022-12-02 09:08:03 +0900

    gql error message

--- client/src/client.tsx
+++ client/src/client.tsx
@@ -1,33 +1,47 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache, HttpLink, from } from '@apollo/client';
 import {
   offsetLimitPagination,
   relayStylePagination,
-} from "@apollo/client/utilities";
+} from '@apollo/client/utilities';
+import { onError } from '@apollo/client/link/error';
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.forEach(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+      )
+    );
+  if (networkError) console.log(`[Network error]: ${networkError}`);
+});
+
+const httpLink = new HttpLink({ uri: 'http://localhost:4000/' });
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000/",
+  link: from([errorLink, httpLink]),
+  uri: 'http://localhost:4000/',
   cache: new InMemoryCache({
     typePolicies: {
       Query: {
         fields: {
-          user: { keyArgs: ["id"] },
+          user: { keyArgs: ['id'] },
         },
       },
       User: {
         fields: {
           offsetTodos: {
             ...offsetLimitPagination([
-              "id",
-              "search",
-              "orderBy",
-              ["text", "completed"],
+              'id',
+              'search',
+              'orderBy',
+              ['text', 'completed'],
             ]),
           },
           cursorTodos: relayStylePagination([
-            "id",
-            "search",
-            "orderBy",
-            ["text", "completed"],
+            'id',
+            'search',
+            'orderBy',
+            ['text', 'completed'],
           ]),
         },
       },
